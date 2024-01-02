1c41519a8b63b8c8fbb08a6b2ab555b70288df8c
redbirdfan <redbirdfan38@gmail.com>
2024-01-01 20:51:42 -0600

    Calc visual is complete

---
+++ mathFunctions.js
@@ -0,0 +1,25 @@
+function multiply(a, b) {
+    a * b == a
+    return a
+}
+
+function addition(a, b) {
+    a + b == a
+    return a
+}
+
+function subtract(a, b) {
+    a - b == a
+    return a
+}
+
+function division(a, b) {
+    a/b == a
+    return a 
+}
+
+function exp(a) {
+    a * a == a
+    return a
+}
+
